b154644c0ec784b8699e6d2de930c9e85b785202
zapalap <przemyslaw.zapala@gmail.com>
2020-08-24 23:29:23 +0200

    Rename attribute and add required params

--- Zapalap.Erg.Cli/Verbs/DiscoverOptions.cs
+++ Zapalap.Erg.Cli/Verbs/DiscoverOptions.cs
@@ -8,7 +8,7 @@ namespace Zapalap.Erg.Cli.Verbs
     [Verb("discover", HelpText = "Discover possible Erg endpoints in a given url")]
     public class DiscoverOptions
     {
-        [Value(0, MetaName = "Url", HelpText ="Url with discoverable Erg endpoints")]
+        [Value(0, MetaName = "Url", HelpText ="Url with discoverable Erg endpoints", Required = true)]
         public string Url { get; set; }
     }
 }
